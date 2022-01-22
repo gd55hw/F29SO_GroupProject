b4aeacbc7204f7b200c435cb5d72e3add6a55ce1
hj18 <Haia_Jihan@hotmail.com>
2022-01-22 02:28:06 +0000

    trying to add  a hook, not working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import Reaact from "react";
+import Reaact, { useState } from "react";
 import './App.css';
 import Header from "./components/Header";
 
+function windowSizeChange() {
+  const [size, setSize] = useState([window.innerHeight, window.innerWidth]);
+  return size;
+}
+
 
 function App() {
+  const [height, width] = windowSizeChange();
+
   return (
     <div className="app">
       <Header />
