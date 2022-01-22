dbe4497800369612f8c9d7bda2467101f296a50e
hj18 <Haia_Jihan@hotmail.com>
2022-01-22 01:41:56 +0000

    Adding, items and so on to the header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Reaact from "react";
 import './App.css';
+import Header from "./components/Header";
+
 
 function App() {
   return (
     <div className="app">
       <h1>Building my application website</h1>
+    
+      <Header />
+      
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -7,9 +7,19 @@ function Header() {
           each part will have items inside of it such as tabs, logo and so on
         */
         <div className="header">
-            <div className="header__left"></div>
+            <div className="header__left">
+               <img src="/images/logo.PNG" alt=""></img>
+                <div className="SearchBar__input" >
+                    <input type="text"></input>
+                </div>
+            </div>
 
-            <div className="header__middle"></div>
+            <div className="header__middle">
+                <a href="Home" className="Header__item">Home</a>
+                <a href="MarketPlace" className="Header__item">Market Place</a>
+                <a href="DoodleCollab" className="Header__item">Doodle Collab</a>
+                <a href="Settings" className="Header__item">Settings</a>
+            </div>
 
             <div className="header__right"></div>
         </div>
