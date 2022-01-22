0c4924f8c574ff77562d287533b58245e91ca7a0
hj18 <Haia_Jihan@hotmail.com>
2022-01-22 00:43:25 +0000

    Working on the header, setting it up

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
-import React from 'react' 
+import React from 'react'; 
+import './Header.css';
 
 function Header() {
     return (
-        <div>
+        /*making the header, we need to divide the header into three parts 
+          each part will have items inside of it such as tabs, logo and so on
+        */
+        <div className="header">
+            <div className="header__left"></div>
 
+            <div className="header__middle"></div>
+
+            <div className="header__right"></div>
         </div>
     )
 }
